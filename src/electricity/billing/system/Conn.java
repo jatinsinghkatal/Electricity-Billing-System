917e979ddc73feeae7203962295f8d15b7174fa2
jatinsinghkatal <jatinsinghkatal@gmail.com>
2024-02-06 02:04:36 +0530

    ebs

---
+++ src/electricity/billing/system/Conn.java
@@ -0,0 +1,18 @@
+package electricity.billing.system;
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+    Conn() {
+        try {
+            c = DriverManager.getConnection("jdbc:mysql:///ebs", "root", "jatinsingh");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+            
+        }
+    }
+}
